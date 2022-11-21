2e6e5a5051f07a32b564c1a4a8c4cf18731406b7
Mertensoid <matsoukm@yandex.ru>
2022-11-21 16:01:49 +0300

    Initial Commit

---
+++ GoogleMapsTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoogleMapsTracker
+//
+//  Created by admin on 21.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
