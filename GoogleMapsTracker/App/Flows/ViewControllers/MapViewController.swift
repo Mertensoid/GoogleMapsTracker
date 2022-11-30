bcbf5a6a19187a3657d9b9ad837736ed9bbd7f99
Mertensoid <matsoukm@yandex.ru>
2022-11-30 17:58:38 +0300

    1. Настроено слежение за перемещением в фоне. 2. Добавлены кнопки «Начать новый трек», «Закончить трек» и «Отобразить предыдущий маршрут». 3. При нажатии на «Начать новый трек»: a. Запускается слежение. b. Создаётся новая линия на карте или заменяется предыдущая. c. При получении новой точки она добавляется в маршрут.

--- GoogleMapsTracker/App/Flows/ViewControllers/MapViewController.swift
+++ GoogleMapsTracker/App/Flows/ViewControllers/MapViewController.swift
@@ -13,8 +13,11 @@ class MapViewController: UIViewController {
     @IBOutlet weak var increaseScaleButton: UIButton!
     @IBOutlet weak var decreaseScaleButton: UIButton!
     @IBOutlet weak var currentLocationButton: UIButton!
+    @IBOutlet weak var newTrackButton: UIButton!
+    @IBOutlet weak var stopTrackButton: UIButton!
+    @IBOutlet weak var loadPreviousTrackButton: UIButton!
     
-    @IBAction func decreaseScale(_ sender: Any) {
+    @IBAction func decreaseScale(_ sender: UIButton) {
         scale -= 1
         let camera = GMSCameraPosition.camera(withTarget: mapView.camera.target, zoom: Float(scale))
         mapView.camera = camera
@@ -24,8 +27,21 @@ class MapViewController: UIViewController {
         let camera = GMSCameraPosition.camera(withTarget: mapView.camera.target, zoom: Float(scale))
         mapView.camera = camera
     }
-    @IBAction func goToCurrentLocation(_ sender: Any) {
+    @IBAction func goToCurrentLocation(_ sender: UIButton) {
         mapView.animate(toLocation: currentCoordinate)
+    }
+    @IBAction func startNewTrack(_ sender: UIButton) {
+        routePoints.removeAll()
+        locationManager?.requestLocation()
+        route?.map = nil
+        route = GMSPolyline()
+        routePath = GMSMutablePath()
+        route?.map = mapView
+        locationManager?.startUpdatingLocation()
+    }
+    @IBAction func stopTrack(_ sender: UIButton) {
+    }
+    @IBAction func loadPreviousTrack(_ sender: UIButton) {
     }
     
     var currentCoordinate = CLLocationCoordinate2D(
@@ -35,25 +51,30 @@ class MapViewController: UIViewController {
         latitude: 37.34033264974476,
         longitude: -122.06892632102273)
     var scale = 15
-//    let coordinate = CLLocationCoordinate2D(
-//        latitude: 37.34033264974476,
-//        longitude: -122.06892632102273)
     var locationManager: CLLocationManager?
+    var backgroundTask: UIBackgroundTaskIdentifier?
+    
+    var route: GMSPolyline?
+    var routePath: GMSMutablePath?
+    var routePoints = [CLLocation]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureMap()
         configureLocationManager()
+        configureBackground()
     }
     
     private func configureLocationManager() {
         
         locationManager = CLLocationManager()
         locationManager?.delegate = self
+        locationManager?.allowsBackgroundLocationUpdates = true
+        locationManager?.startMonitoringSignificantLocationChanges()
+        locationManager?.pausesLocationUpdatesAutomatically = false
+        
         locationManager?.requestAlwaysAuthorization()
-        locationManager?.startUpdatingLocation()
-        //locationManager?.allowsBackgroundLocationUpdates = true
     }
     
     private func configureMap() {
@@ -61,9 +82,23 @@ class MapViewController: UIViewController {
         let camera = GMSCameraPosition.camera(withTarget: currentCameraCoordinate, zoom: Float(scale))
         mapView.camera = camera
         mapView.isMyLocationEnabled = true
+        
         increaseScaleButton.layer.cornerRadius = 25
         decreaseScaleButton.layer.cornerRadius = 25
         currentLocationButton.layer.cornerRadius = 25
+        newTrackButton.layer.cornerRadius = 25
+        stopTrackButton.layer.cornerRadius = 25
+        loadPreviousTrackButton.layer.cornerRadius = 25
+    }
+    
+    private func configureBackground() {
+        backgroundTask = UIApplication.shared.beginBackgroundTask { [weak self] in
+            guard let self = self,
+                  let backgroundTask = self.backgroundTask else { return }
+            
+            UIApplication.shared.endBackgroundTask(backgroundTask)
+            self.backgroundTask = .invalid
+        }
     }
 }
 
@@ -76,11 +111,17 @@ extension MapViewController: CLLocationManagerDelegate {
         guard let location = locations.last else { return }
         
         currentCoordinate = location.coordinate
-        
-        let marker = GMSMarker(position: location.coordinate)
-        marker.map = mapView
+
         let camera = GMSCameraPosition.camera(withTarget: currentCoordinate, zoom: Float(scale))
         mapView.camera = camera
+        
+        routePath?.add(location.coordinate)
+        routePoints.append(location)
+        route?.path = routePath
+        
         print(location.coordinate)
+    }
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
     }
 }
